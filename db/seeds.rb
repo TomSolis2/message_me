6fe17d50530f483e6b201a2c9017096f1f50873f
santost.solis <tsoli198@gmail.com>
2021-12-02 16:00:02 -0600

    Build Message resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Elvira", password: "Tigr9cat")
-User.create(username: "Steven", password: "Tigr9cat")
-User.create(username: "Eddie", password: "Tigr9cat")
-User.create(username: "Audrey", password: "Tigr9cat")
-User.create(username: "Matthew", password: "Tigr9cat")
+  
